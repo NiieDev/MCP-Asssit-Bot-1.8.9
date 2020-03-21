c65b8628097d52ac91f886f1417b16be4dc3014a
Niie <43474785+NiieDev@users.noreply.github.com>
2020-03-21 23:25:10 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('message', message => {
     }
     else if (message.content.startsWith(prefix + 'mcptr')) {
         let field_or_method = message.content.split(" ").slice(1);
-        message.channel.send("```" + field_or_method + ": " + fields.get("field_175514_bm") + ", " + fields.get(field_or_method) + "```");
+        message.channel.send("```" + field_or_method + ": " + fields.get("field_175514_bm") + ", " + fields.get(field_or_method + "") + "```");
     }
 });
 
